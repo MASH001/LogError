8941561a62378b68401233c0c871ce4a107c96f5
MASH001 <41975321+MASH001@users.noreply.github.com>
2020-04-29 20:08:55 -0400

    Add files via upload

---
+++ LogError.cs
@@ -0,0 +1,42 @@
+﻿using CustomerRejCompInvest.Models;
+using CustomerRejCompInvest.Models.DBContext;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+
+public class LogError : ILogException
+{
+    private string UserName { get; set; }
+    private string ApplicationName { get; set; }
+    //private UserAccessDbContext _dbContext;
+    private quality_controlEntities2 _dbContext;
+
+    //public LogError(UserAccessDbContext dbContext)
+    //{
+    //    _dbContext = dbContext;
+    //}
+  
+    public LogError(quality_controlEntities2 dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+
+    public void LogErrorFileSystem(System.Exception ex, string action)
+    {
+        using (StreamWriter writer = new StreamWriter(@"\\10.10.7.217\\NASShare\WEBLogs\" + ApplicationName + ".txt", true))
+        {
+            writer.WriteLine(DateTime.Now.ToString() + ": UserName: " + UserName + ": Action:" + action + ": " + ex.Message + ": \r\nStack Trace: " + ex.StackTrace + ":");
+        }
+        // throw new NotImplementedException();
+    }
+
+    public int LogErrorDB(System.Exception ex, string action)
+    {
+        throw new NotImplementedException();
+
+    }
+}
