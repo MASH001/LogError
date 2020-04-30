aa21dd4b9e9a4c3a54526b15cda2eb1ca9823555
MASH001 <41975321+MASH001@users.noreply.github.com>
2020-04-29 20:13:10 -0400

    Add files via upload

---
+++ ILogException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+   public interface ILogException
+    {
+    int LogErrorDB(Exception ex, string action);
+    void LogErrorFileSystem(Exception ex, string action);
+
+    }
+
